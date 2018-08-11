8a0b9dd3015047f26094ae9739a782cc85b2565c
chfw <wangc_2011@hotmail.com>
2018-08-11 22:38:06 +0100

    :art: update version number

--- setup.py
+++ setup.py
@@ -21,14 +21,14 @@ PY26 = PY2 and sys.version_info[1] < 7
 
 NAME = 'echarts-themes-pypkg'
 AUTHOR = 'C.W.'
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 EMAIL = 'wangc_2011@hotmail.com'
 LICENSE = 'git push -u origin masterMIT'
 DESCRIPTION = (
     'packages echarts vintage, macarons, infographic, shine, roma themes'
 )
 URL = 'https://github.com/pyecharts/echarts-themes-pypkg'
-DOWNLOAD_URL = '%s/archive/0.0.1.tar.gz' % URL
+DOWNLOAD_URL = '%s/archive/0.0.2.tar.gz' % URL
 FILES = ['README.rst', 'CHANGELOG.rst']
 KEYWORDS = [
     'python'
@@ -49,7 +49,7 @@ CLASSIFIERS = [
 ]
 
 INSTALL_REQUIRES = [
-    'lml==0.0.2',
+    'lml>=0.0.2',
     'pyecharts-jupyter-installer==0.0.3',
 ]
 SETUP_COMMANDS = install_cmd_for(
@@ -62,8 +62,8 @@ EXTRAS_REQUIRE = {
 # You do not need to read beyond this line
 PUBLISH_COMMAND = '{0} setup.py sdist upload -r pypi'.format(
     sys.executable)
-GS_COMMAND = ('gs echarts-themes-pypkg v0.0.1 ' +
-              "Find 0.0.1 in changelog for more details")
+GS_COMMAND = ('gs echarts-themes-pypkg v0.0.2 ' +
+              "Find 0.0.2 in changelog for more details")
 NO_GS_MESSAGE = ('Automatic github release is disabled. ' +
                  'Please install gease to enable it.')
 UPLOAD_FAILED_MSG = (
